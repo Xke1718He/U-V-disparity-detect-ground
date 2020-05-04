f5cb04071d79a6ef098e5fb67fbbdf5bd86cf544
XKe_He <41565215+Xke1718He@users.noreply.github.com>
2020-05-04 14:43:13 +0800

    Add files via upload

---
+++ detect_ground_v1/cmake-build-debug/CMakeFiles/detect_ground.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/detect_ground.dir/main.cpp.o"
+  "CMakeFiles/detect_ground.dir/src/DetectGround.cpp.o"
+  "detect_ground"
+  "detect_ground.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/detect_ground.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
